01f48ed30f94fe27b4aefdb9e54128c8418e494e
Erik Seki <56083781+erikseki@users.noreply.github.com>
2022-04-05 14:00:48 -0300

    metodo de retorno

--- jsObj/repetiçãoDeCodigo/index.js
+++ jsObj/repetiçãoDeCodigo/index.js
@@ -7,19 +7,22 @@ class Cliente{
 
 class ContaCorrente{
     agencia;
-    #saldo =0;
+    saldo =0;
 
     sacar(valor){
-        if(this.#saldo >= valor) {
-            this.#saldo -= valor;
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return valor;
         }
  
     }
 
     depositar(valor){
-        if(valor > 0){
-            this.saldo += valor;      
+        if(valor <= 0){
+            return; // early return - condição antecipada para o metodo parar caso for menor que 0
         }
+
+        this.saldo += valor;      
     }
 }
 
@@ -37,13 +40,13 @@ cliente2.cpf = 88822233309;
 
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.#saldo = 10000;
+contaCorrenteRicardo.saldo = 10000;
 contaCorrenteRicardo.agencia = 1001;
 
 
 contaCorrenteRicardo.depositar(100);
 
-contaCorrenteRicardo.sacar(50);
-
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
