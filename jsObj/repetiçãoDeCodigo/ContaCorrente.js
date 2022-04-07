527347c11b3a5e205c0add912433ba850cadce58
Erik Seki <56083781+erikseki@users.noreply.github.com>
2022-04-07 20:36:29 -0300

    Tipo de Valor e Tipo de Referência  📦

--- jsObj/repetiçãoDeCodigo/ContaCorrente.js
+++ jsObj/repetiçãoDeCodigo/ContaCorrente.js
@@ -19,9 +19,13 @@ export class ContaCorrente{
         this.saldo += valor;      
     }
 
-    transferir(valor, conta){
+    transferir(valor, conta){ 
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        
+        // valor = 20; - do tipo primitivo e sendo um valor, se eu passar um valor aqui dentro da função, ela passa como 
+        // uma copia, o valor aqui dentro n reflete no escopo la do index.js (continua sendo 200)
+
+        // conta.cidade = "Sao paulo"; - contudo o parametro "Conta", ela é um objeto do tipo de referencia, qualquer valor 
+        //aqui dentro reflete la fora 
     }
 }
