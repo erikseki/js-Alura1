4ecd78af6d9f96f59f94325d257935f8b0f41c6c
Erik Seki <56083781+erikseki@users.noreply.github.com>
2022-04-11 17:06:50 -0300

    Null e Undefined  📦

--- jsObj/repetiçãoDeCodigo/index.js
+++ jsObj/repetiçãoDeCodigo/index.js
@@ -7,10 +7,9 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
 
-const cliente2 = new Cliente();
-
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+// const cliente2 = new Cliente();
+// cliente2.nome = "Alice";
+// cliente2.cpf = 88822233309;
 
 
 const contaCorrenteRicardo = new ContaCorrente();
@@ -20,11 +19,14 @@ contaCorrenteRicardo.depositar(500);
 
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = new Cliente(); // conta2.cliente = null); 
+conta2.cliente.nome = "Alice";
+conta2.cliente.cpf = 88822233309;
 conta2.agencia = 102;
 
 let valor = 200;
-contaCorrenteRicardo.transferir(200, conta2);
+contaCorrenteRicardo.transferir(valor, conta2);
+
 
 console.log(conta2);
 // console.log(contaCorrenteRicardo);
